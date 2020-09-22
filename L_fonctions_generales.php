a164c00e2ead07eaff2f221a55c0304443ff1918
nicolas <nicolascaulier@gmail.com>
2020-09-22 11:40:29 +0200

    loginfuad

--- L_fonctions_generales.php
+++ L_fonctions_generales.php
@@ -1,1 +1,30 @@
+<?php
+function gestionSession()
+{
+    GLOBAL $inscription, 
+    
+    session_start();
+    if (isset($_SESSION[""]))
+}
+function setHeaderNoCache()
+{
+	GLOBAL $__URL_local;
+	
+	echo "<DOCTYPE html>\n";
+	echo "<html>\n";
+	echo "<head>\n";
+	echo "<meta http-equiv=\"Cache-Control\" content=\"no-cache, no-store, must-revalidate\" />\n";
+	echo "<meta http-equiv=\"Pragma\" content=\"no-cache\" />\n";
+	echo "<meta http-equiv=\"Expires\" content=\"0\" />\n";
+	echo "<link href=\"annonce.css\" rel=\"stylesheet\">\n";
+	//echo "<link href=\"MA_CSS.css\" rel=\"stylesheet\">\n";
+	echo "<script>\n";
+	echo "function goAffGrand( id )\n";
+	echo "{ window.location.replace(\"http://localhost$__URL_local/login.php?IDinscription=\"+id );}\n";
+	echo "</script>\n";
+	echo "</head>\n";
+	echo "<body>\n";
+}
 
+
+?>
--- login.php
+++ login.php
@@ -1,1 +1,25 @@
 
+<?php
+include (L_fonctions_generales.php);
+
+session start();
+$user = $_SESSION['users'];
+$users = $_SESSION['users'];
+
+
+
+echo " votre login est $user ";
+
+
+if($_POST)
+    if (isset($_POST|'login')) && isset($_POST['pwd'])
+    {
+        $nom = $_POST['nom'];
+        $pwd = $_POST['password'];
+        $_SESSION['user'] = $login;
+
+        return( [ $nom, $pwd]);
+    }
+
+
+?>
